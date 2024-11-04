a65f6ab916bff43e68aa64f1f22297ab733165f6
satyve <152157314+satyve@users.noreply.github.com>
2024-11-04 14:51:52 +0100

    oppskrift til prosjektet

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+# Oppskrift 
+# - finne dna datasett
+# - lage program som leser datasett
+# - lage prpogroam som krypterer og lager komplimentert RNA-molekyl som tekst eller liste
+# - oversikt over aminosyrer i riktig rekkefølge 
+# - stoppe programmet om man finner stopp kodon. 
+
