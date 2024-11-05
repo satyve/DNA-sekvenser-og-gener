de5aa9cfc1f16cc640f5b78f08e8ad487f1aa2da
ElineWr <elinewr@viken.no>
2024-11-05 07:04:24 +0100

    Oppdata Aminosyrene
    Gjorde om tymin(T) til uracil(U). Jeg kommenterte også bort en del vi må se på

--- main.py
+++ main.py
@@ -11,28 +11,30 @@ with open(inputfile, "r") as f:
 # Fjerner linjeskift
 seq = seq.replace("\n", "").replace("\r", "")
 
+""""
 def dna_to_rna(dna_seq):
     return dna_seq.replace("T", "U").replace("A", "U").replace("C", "G").replace("G", "C")
 print(dna_seq)
+"""
 
 def translate(seq): 
     table = { 
-        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
-        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
-        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
-        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                 
-        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
-        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
-        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
-        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
-        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
-        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
-        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
-        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
-        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
-        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
-        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
-        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+        'AUA':'I', 'AUC':'I', 'AUU':'I', 'AUG':'M', 
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACU':'T', 
+        'AAC':'N', 'AAU':'N', 'AAA':'K', 'AAG':'K', 
+        'AGC':'S', 'AGU':'S', 'AGA':'R', 'AGG':'R',                 
+        'CUA':'L', 'CUC':'L', 'CUG':'L', 'CUU':'L', 
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P', 
+        'CAC':'H', 'CAU':'H', 'CAA':'Q', 'CAG':'Q', 
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGU':'R', 
+        'GUA':'V', 'GUC':'V', 'GUG':'V', 'GUU':'V', 
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A', 
+        'GAC':'D', 'GAU':'D', 'GAA':'E', 'GAG':'E', 
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGU':'G', 
+        'UCA':'S', 'UCC':'S', 'UCG':'S', 'UCU':'S', 
+        'UUC':'F', 'UUU':'F', 'UUA':'L', 'UUG':'L', 
+        'UAC':'Y', 'UAU':'Y', 'UAA':'_', 'UAG':'_', 
+        'UGC':'C', 'UGU':'C', 'UGA':'_', 'UGG':'W', 
     } 
     protein ="" 
     for i in range(0, len(seq) - len(seq) % 3, 3): 
