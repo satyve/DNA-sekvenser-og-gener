2ad7c66bef3a6aad1b4c7043e65ac7a5f33baab6
ElineWr <elinewr@viken.no>
2024-11-26 15:50:29 +0100

    HOLA! tørker våt kode 😊
    Egentlig akkurat det vi hadde fra før av, men nå i stilig funksjons form i biblioteket

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
-from bibliotek import dna_to_rna, oversett, find_protein, proteiner, visualiser_protein_sekvens
+from bibliotek import dna_to_rna, oversett, find_protein, proteiner, visualiser_protein_sekvens, analyser_protein
 
 # Les DNA-sekvens fra fil
 inputfile = "DNA.txt"
 with open(inputfile, "r") as f:
     seq = f.read()
 
-# Fjerner linjeskift
-seq = seq.replace("\n", "").replace("\r", "")
-
-# Konverter DNA til RNA og oversett
-rna_sequence = dna_to_rna(seq)
-protein_sequence = oversett(rna_sequence)
-
-# Finn proteiner
-de_funnet = find_protein(protein_sequence)
-print("Funnet proteiner:", de_funnet)
-find_protein(protein_sequence)
-
-# Lar brukeren velge et protein fra listen over funn
-print("Velg et protein fra listen:")
-for i, navn in enumerate(de_funnet):  # Viser tilgjengelige valg med nummerering
-    print(f"{i + 1}. {navn}")
-
-valg = int(input("\nSkriv nummeret til proteinet du vil visualisere: ")) - 1  # Brukerens valg
-protein_navn = de_funnet[valg]  # Henter navnet på proteinet basert på brukerens valg
-protein_sekvens = proteiner[protein_navn]  # Finner proteinsekvensen for det valgte proteinet
-
-# Funksjon for å visualisere proteinsekvensen som en 3D-spiral med etiketter
+protein_navn, protein_sekvens = analyser_protein(seq, dna_to_rna, oversett, find_protein, proteiner)
 
 # Visualiserer den valgte proteinsekvensen
 visualiser_protein_sekvens(protein_sekvens, protein_navn, "nei")
